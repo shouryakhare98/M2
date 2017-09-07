dd748cb5182285fbf8b1875fbdff963f31b633a1
Lane Homrich <LaneHomrich@lawn-143-215-124-233.lawn.gatech.edu>
2017-09-07 17:56:37 -0400

    Updated Person2

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -11,14 +11,16 @@ package edu.gatech.oad.antlab.person;
 public class Person2 {
     /** Holds the persons real name */
     private String name;
-	 	/**
+
+    /**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
+	    public Person2(String pname) {
+	        name = pname;
+	    }
+
 	/**
 	 * This method should take the string
 	 * input and return its characters in
@@ -29,10 +31,19 @@ public class Person2 {
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
-	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
-	}
+    private String calc(String input) {
+        String randomized = "";
+        int index;
+        char character;
+        while (input.length() != 0) {
+            index = (int) Math.floor(Math.random() * input.length());
+            character = input.charAt(index);
+            input = input.substring(0, index) + input.substring(index + 1);
+            randomized = randomized + character;
+        }
+        return randomized;
+    }
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
