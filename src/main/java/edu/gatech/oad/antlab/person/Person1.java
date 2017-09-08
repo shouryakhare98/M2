9635fadd69e29422a2f56bb9a409f9dd9c3eb5c7
shouryakhare98 <shouryakhare@lawn-128-61-1-48.lawn.gatech.edu>
2017-09-08 14:00:01 -0400

    Person1 fixed

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -32,15 +32,15 @@ public class Person1 {
 	private String calc(String input) {
 		String part1 = input.substring(0,2);
 		String part2 = input.substring(2);
-	  return (part1 + part2);
+	  return (part2 + part1);
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
